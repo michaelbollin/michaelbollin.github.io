de4a4e0b33d2dd5cadc56ff0e07a24eef99579cc
michaelbollin <michael@littlebigmake.com>
2019-08-31 01:07:13 +0200

    Buttons + homepage

---
+++ buttons/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My Portfolio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--[if IE]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+    <link rel="stylesheet" href="stylesheets/style.css?<?php echo time(); ?>" />
+    <link rel="stylesheet" href="/common.css" />
+</head>
+
+<body>
+<div id="container">
+	<div id="sidebar">
+  <p><a href="http://justcode.today">&larr; back to blog</a></p>
+	<h1 class='logowrapper'>
+    <a href="/"><div class="logo">JUST CODE <span class="apps">APPS</span></div>
+    <div class="tagline">Pure CSS Button library</div></a>
+	</h1>
+
+    <p class="copy">
+        A library of pure CSS buttons free to use. Click the button to get the code or see it isolated in Codepen.
+    </p>
+
+    <ul class="list">
+        <li><a href="#"><span>Flat buttons</span></a></li>
+        <li><a href="#"><span>Rainbow buttons</span></a></li>
+    </ul>
+
+    </div>
+
+    <div class="wrap">
+    
+        <h2>Flat buttons</h2>
+        <button class="b1"><span>Gradient</span></button>
+        <button class="b2"><span>Arrow</span></button>
+        <button class="b3"><span>Circle</span><i class="circle"></i></button>
+        <button class="b4"><span>Loader</span></button>
+        
+        <button class="b6"><i class="star"></i><span>Flip</span></button>
+        <button class="b7"><i class="star"></i><span>Rotate</span></button>
+
+        <button class="b10"><i class="star"></i><span>Blur</span></button>
+        <button class="b11"><i class="star"></i><span>Bounce out</span></button>
+
+        <button class="b5"><span>White</span></button>
+        <button class="b8"><span>Ghost</span></button>
+        <button class="b9"><span><i>Frame</i></span></button>
+        <button class="b12"><span><i>Lines</i></span></button>
+    </div>
+
+
+</div>
+
+    <script> </script>
+</body>
+
+</html>
